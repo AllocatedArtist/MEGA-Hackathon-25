03ea22193a329bfb948f8f1d70dc7e70a532aa29
Allocated Artist <Senorsiete2006@gmail.com>
2025-03-16 02:09:25 -0700

    supply ui

--- src/business.rs
+++ src/business.rs
@@ -2,23 +2,35 @@ use std::collections::HashMap;
 
 use notan_egui::*;
 
+#[derive(Clone)]
 enum Quality {
     Basic,
     Good,
     Exceptional,
 }
 
+#[derive(Clone)]
 struct Product {
     quantity: i32,
     quality: Quality,
     production_cost: i32,
 }
 
+impl Product {
+    fn base(cost: i32) -> Self {
+        Self {
+            quantity: 0,
+            quality: Quality::Basic,
+            production_cost: cost,
+        }
+    }
+}
+
 pub struct Business {
     funds: i32,
     prices: HashMap<String, i32>,
     allocation: HashMap<String, i32>,
-    quantities: HashMap<String, i32>,
+    quantities: HashMap<String, Product>,
 }
 
 impl Business {
@@ -51,16 +63,16 @@ impl Business {
         allocation.insert(String::from("Marketing"), 0);
 
         let mut quantities = HashMap::new();
-        quantities.insert(String::from("Food"), 0);
+        quantities.insert(String::from("Food"), Product::base(10));
 
-        quantities.insert(String::from("Fighter Armor"), 0);
-        quantities.insert(String::from("Fighter Weapons"), 0);
+        quantities.insert(String::from("Fighter Armor"), Product::base(100));
+        quantities.insert(String::from("Fighter Weapons"), Product::base(75));
 
-        quantities.insert(String::from("Cleric Armor"), 0);
-        quantities.insert(String::from("Cleric Weapons"), 0);
+        quantities.insert(String::from("Cleric Armor"), Product::base(100));
+        quantities.insert(String::from("Cleric Weapons"), Product::base(75));
 
-        quantities.insert(String::from("Mage Armor"), 0);
-        quantities.insert(String::from("Mage Weapons"), 0);
+        quantities.insert(String::from("Mage Armor"), Product::base(100));
+        quantities.insert(String::from("Mage Weapons"), Product::base(75));
 
         Self {
             funds: initial_fund,
@@ -142,6 +154,20 @@ impl Business {
         );
     }
 
+    pub fn update_quantities(&mut self) {
+        for (k, v) in self.quantities.iter_mut() {
+            let fund_percentage = self.allocation.get(k).unwrap().clone();
+            let fund_percentage = fund_percentage as f32 * 0.01;
+            let fund = (self.funds as f32 * fund_percentage) as i32;
+
+            let unit_production = fund / v.production_cost;
+
+            v.quantity += unit_production;
+            self.funds -= fund;
+            self.funds = self.funds.max(0);
+        }
+    }
+
     pub fn show_allocation(&mut self, ui: &mut Ui) {
         ui.label(format!("Available Funds: ${}", self.funds()));
 
@@ -167,5 +193,45 @@ impl Business {
 
         ui.separator();
         self.allocation_label(ui, "Marketing");
+    }
+
+    fn supply_label(&self, ui: &mut Ui, name: &str) {
+        let product = self.quantities[name].clone();
+
+        let quality = match product.quality {
+            Quality::Basic => "Basic",
+            Quality::Good => "Good",
+            Quality::Exceptional => "Exceptional",
+        };
+
+        let tooltip_format = format!(
+            "Quality: {}\nProduction Cost ($): {}",
+            quality, product.production_cost
+        );
+
+        ui.label(format!(
+            "{} (Units): {}",
+            name, self.quantities[name].quantity
+        ))
+        .on_hover_text(tooltip_format);
+    }
+
+    pub fn show_supply(&self, ui: &mut Ui) {
+        self.supply_label(ui, "Food");
+
+        ui.separator();
+
+        self.supply_label(ui, "Fighter Armor");
+        self.supply_label(ui, "Fighter Weapons");
+
+        ui.separator();
+
+        self.supply_label(ui, "Cleric Armor");
+        self.supply_label(ui, "Cleric Weapons");
+
+        ui.separator();
+
+        self.supply_label(ui, "Mage Armor");
+        self.supply_label(ui, "Mage Weapons");
     }
 }
